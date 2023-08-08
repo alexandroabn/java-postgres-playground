2875ad4f16513a01c63a3912aaa274e0dca07704
Alexandro Nascimento <alexandroabn@gmail.com>
2023-08-08 17:16:45 +0000

    Concluindo o AppBb

--- src/main/java/com/example/AppBd.java
+++ src/main/java/com/example/AppBd.java
@@ -9,17 +9,17 @@ public class AppBd {
     private static final String PASSWORD = "";
     private static final String USERNAME = "gitpod";
     private static final String JDBC_URL = "jdbc:postgresql://localhost/postgres";
-    private Connection conn;
+    //private Connection conn;
 
     public static void main(String[] args) {
-           new AppBd();
+        new AppBd();
     }
 
     public AppBd () {
         try (var conn = getConnection()){
-            carregarDriverJDBC();
-            listarEstados();
-            localizarEstado("TO");
+            listarEstados(conn);
+            localizarEstado(conn, "TO");
+            listarDadosTabela(conn, "cliente");
         } catch (SQLException e) {
             System.err.println("Não foi possível conectar ao banco de dados: " + e.getMessage());
         }
@@ -27,28 +27,59 @@ public class AppBd {
     }
 
 
-    private void listarEstados() {
-        
+    private void listarDadosTabela(Connection conn, String tabela) {
+        var sql = "select * from " + tabela;
+        try {
+            var statement = conn.createStatement();
+            var result = statement.executeQuery(sql);
+            
+            var metadata =  result.getMetaData();
+            int cols =  metadata.getColumnCount();
 
-        Statement statement = null;
-        try (var conn = getConnection()){
-                System.out.println("conexão com o banco realizada com sucesso");
-
-                statement = conn.createStatement();
-                var result = statement.executeQuery("select * from estado");
-                    while(result.next()){
-                        System.out.printf("Id: %d Nome: %s UF: %s\n", result.getInt("id"), result.getString("nome"), result.getString("uf"));
+            for (int i = 1; i < cols; i++) {
+                System.out.printf("%-25s |", metadata.getColumnName(i));
+            }
+            System.out.println();
+            
+            while(result.next()){
+                
+                for (int i = 1; i < cols; i++) {
+                    System.out.printf("%-25s |", result.getString(i));
                 }
-
+                System.out.println();
+            }
         } catch (SQLException e) {
-            if (statement == null)
-                System.err.println("Não foi possível conectar ao banco de dados: " + e.getMessage());
-            else 
-                System.err.println("Não foi possível executar a consulta ao banco: " + e.getMessage());
+            System.out.println("Erro na execução da consulta :" + e.getMessage());
         }
     }
 
-    private void localizarEstado(String uf) {
+    private void listarEstados(Connection conn) {
+        Statement statement = null;
+        try{
+            System.out.println("conexão com o banco realizada com sucesso");
+
+            statement = conn.createStatement();
+            var result = statement.executeQuery("select * from estado");
+            while(result.next()){
+                System.out.printf("Id: %d Nome: %s UF: %s\n", result.getInt("id"), result.getString("nome"), result.getString("uf"));
+            }
+        } catch (SQLException e) {
+            System.err.println("Não foi possível executar a consulta ao banco: " + e.getMessage());
+        }
+    }
+
+    private void localizarEstado(Connection conn, String uf) {
+        try {
+            var sql = "select * from estado where uf = ?";
+            var statement = conn.prepareStatement(sql);
+            statement.setString(1, uf);
+            var result = statement.executeQuery();
+            if(result.next()){
+                System.out.printf("Id: %d Nome: %s UF: %s\n", result.getInt("id"), result.getString("nome"), result.getString("uf"));
+            }
+        } catch (SQLException e) {
+            System.err.println("Não foi possível executar a consulta ao banco: " + e.getMessage());
+        }
     }
 
     private Connection getConnection() throws SQLException{
